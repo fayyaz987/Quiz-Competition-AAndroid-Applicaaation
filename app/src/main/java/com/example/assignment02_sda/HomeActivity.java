0b4961c87f45ad9df1056844cda83a2be8db2527
MUHAMMAD WAQAS FAYYAZ <samiwaqas567@outlook.com>
2019-05-26 17:32:59 +0500

    HomeScreen Bckend -QC

--- app/src/main/java/com/example/assignment02_sda/HomeActivity.java
+++ app/src/main/java/com/example/assignment02_sda/HomeActivity.java
@@ -19,11 +19,56 @@ public class HomeActivity extends AppCompatActivity {
 
     TextView tv;
     String n;
-    Button btn,btn2,btn3;
+    Button btn, btn2, btn3;
     SharedPreferences sharedpreferences;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        tv = (TextView) findViewById(R.id.textView1);
+        btn = (Button) findViewById(R.id.button1);
+        btn2 = (Button) findViewById(R.id.button2);
+        38
+        btn3 = (Button) findViewById(R.id.button3);
+        Bundle data = getIntent().getExtras();
+        n = data.getString("Name");
+        tv.setText("Welcome " + n);
+        btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+                sharedpreferences = getSharedPreferences(LogIn.MyPREFERENCES, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.clear();
+                editor.commit();
+                Intent i = new Intent(getApplicationContext(), LogIn.class);
+                startActivity(i);
+            }
+        });
+        btn2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new
+                        Intent(getApplicationContext(), HighScores.class);
+                i.putExtra("Name", n);
+                startActivity(i);
+            }
+        });
     }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("Exit").setMessage("Are you sure?").setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        }).setNegativeButton("no", null).show();
+    }
+
 }
